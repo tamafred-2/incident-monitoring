05aae3123ba563a3f2d89c51a27583b837b54c40
Al Fred <tamafred2@gmail.com>
2026-07-14 08:09:12 +0800

    feat: queue incident update notifications

--- app/Notifications/IncidentUpdatedNotification.php
+++ app/Notifications/IncidentUpdatedNotification.php
@@ -4,9 +4,10 @@ namespace App\Notifications;
 
 use App\Models\Incident;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notification;
 
-class IncidentUpdatedNotification extends Notification
+class IncidentUpdatedNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
